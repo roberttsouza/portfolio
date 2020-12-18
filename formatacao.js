f2bc9f9172d4f73f89163bc40506c94045e5e093
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2020-12-18 17:42:59 -0300

    função dark mode

---
+++ formatacao.js
@@ -0,0 +1,7 @@
+/*função Dark mode*/
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#switch')
+
+$checkbox.addEventListener('change', function() {
+    $html.classList.toggle('dark-mode')
+})
