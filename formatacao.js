be74742e0d4c216a1ae6b3b2143f0a2da0a4eff5
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2020-12-19 00:53:17 -0300

    menu slide, button dark mode
    Realizado a conclusão dos funções de dark mode e menu slide, faltando alguns pequenas ajustes de posicionamento

--- formatacao.js
+++ formatacao.js
@@ -5,3 +5,14 @@ const $checkbox = document.querySelector('#switch')
 $checkbox.addEventListener('change', function() {
     $html.classList.toggle('dark-mode')
 })
+
+
+
+/*Função navbar mobile*/
+function openNav() {
+    document.getElementById("my-menu-nav").style.height = "100vh" ;
+  }
+  
+  function closeNav() {
+    document.getElementById("my-menu-nav").style.height = "0";
+  }
